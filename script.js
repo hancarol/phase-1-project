7071db7946217a5dc5debddacf24ec247e024ab1
Caroline Ko <hancarol91@gmail.com>
2023-02-07 22:20:52 -0500

    added apis

--- script.js
+++ script.js
@@ -22,6 +22,15 @@ function time() {
   ];
 
   let day = days[now.getDay()];
-  h2.innerHTML = `${day} ${hours}:${minutes}`;
+  h2.innerText = `${day} ${hours}:${minutes}`;
 }
 time();
+
+function getForecast(coordinates) {
+  let apiKey = "7e320e70b095384fdeef1e450eb06132";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+}
+function showCity(city) {
+  let apiKey = "7e320e70b095384fdeef1e450eb06132";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+}
